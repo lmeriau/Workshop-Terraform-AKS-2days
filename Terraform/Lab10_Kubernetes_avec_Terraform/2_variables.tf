ccb664750bf5bb5e32df78069c7b60602420febd
Laurent Meriau <60506872+lmeriau@users.noreply.github.com>
2023-01-12 15:35:12 +0100

    Update 2_variables.tf

--- Terraform/Lab10_Kubernetes_avec_Terraform/2_variables.tf
+++ Terraform/Lab10_Kubernetes_avec_Terraform/2_variables.tf
@@ -1,11 +1,11 @@
 #  Resource Group Name du cluster AKS
 variable "resource_group_name" {
   type    = string
-  default = "RG-AKSCluster"
+  default = "RG-AKSCluster-LMU"
 }
 
 # AKS Cluster name
 variable "cluster_name" {
   type    = string
-  default = "AKS-000"
+  default = "AKS-LMU"
 }
